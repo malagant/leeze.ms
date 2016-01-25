e6d6753a546729de21e0c6453bb5df749ef56d71
Michael Johann <mjohann@rails-experts.com>
2016-01-25 10:19:24 +0100

    After adding rolify generated stuff refs: #4_roles_and_admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,10 @@ Category.create(name: 'Radtour')
 Category.create(name: 'Mountainbiketour')
 Category.create(name: 'Radwandern')
 
+pass = SecureRandom.hex(5)
+admin = User.create email: 'admin@example.com',
+                    password: pass,
+                    password_confirmation: pass
+admin.add_role :admin
+puts "Admin password is #{pass}"
+
